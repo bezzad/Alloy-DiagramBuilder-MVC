74f984e89d0a2dd498e7aecddf637421fc123bc4
Behzad Khosravifar <behzad.khosravifar>
2017-01-15 14:00:18 +0330

    add test file

---
+++ src/AlloyDiagram/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlloyDiagram.MvcApplication" Language="C#" %>
---
+++ src/AlloyDiagram/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlloyDiagram.Startup))]
+namespace AlloyDiagram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
