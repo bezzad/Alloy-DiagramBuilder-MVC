240db0215b8b43ff3b3221ac72967968c6d62e8c
Behzad Khosravifar <behzad.khosravifar>
2017-01-17 11:51:17 +0330

    decrease .NET version to 4.5 from 4.6

---
+++ src/AlloyDiagram/Controllers/DiagramController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AlloyDiagram.Controllers
+{
+    public class DiagramController : ApiController
+    {
+        [HttpPost]
+       // [Route(Name = "SaveDiagram")]
+        public IHttpActionResult SaveDiagram([FromBody]string data)
+        {
+            return Ok("OK");
+        }
+    }
+
+    
+}
