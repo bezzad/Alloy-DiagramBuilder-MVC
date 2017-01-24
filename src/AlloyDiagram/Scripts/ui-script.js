ebe3a468e6aff59ed0f3751e18cfaa8fd66c71fd
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2017-01-24 15:54:24 +0330

    Add readonly diagram feature

--- src/AlloyDiagram/Scripts/ui-script.js
+++ src/AlloyDiagram/Scripts/ui-script.js
@@ -1,6 +1,7 @@
-function DiagramDrower(containerId, builderId, nodes, connections, callBackDiagram) {
+function DiagramDrower(containerId, builderId, nodes, connections, isReadonly, callBackDiagram) {
     YUI().use('aui-diagram-builder', function (y) {
         $.getJSON(window.location.origin + "/api/diagram/GetAvailableFields", null, function (data) {
+            if (isReadonly === true) data = [];
             var diagram = new y.DiagramBuilder(
             {
                 availableFields: data,
@@ -12,7 +13,30 @@ function DiagramDrower(containerId, builderId, nodes, connections, callBackDiagr
             diagram.connectAll(connections);
 
             if (callBackDiagram !== undefined) callBackDiagram(diagram);
+
+            if(isReadonly === true) ReadonlyDiagram(diagram);
         });
+    });
+}
+
+/*
+ * Readonly the diagram
+ */
+function ReadonlyDiagram(diagram) {
+    function detachRecursively(node) {
+        node.get('children').each(detachRecursively);
+
+        // You may also want to set the cursor to the default since it will 
+        // change based on which elements the mouse is over.
+        node.setStyle('cursor', 'auto');
+
+        // You may want to detach specific events such as 'click' or 
+        // 'mousedown' if you do not want to disable all events.
+        node.detach();
+    };
+
+    diagram.on('render', function (event) {
+        detachRecursively(diagram.get('boundingBox'));
     });
 }
 
--- src/AlloyUiDiagram/Diagram.cs
+++ src/AlloyUiDiagram/Diagram.cs
@@ -5,6 +5,14 @@ namespace AlloyUiDiagram
 {
     public class Diagram
     {
+        public int DiagramId { get; set; }
+
+        public string DiagramName { get; set; }
+
+        public bool IsReadOnly { get; set; }
+
+        public DateTime ModifyDate { get; set; }
+
         public List<Node> Nodes { get; set; }
     }
 }
